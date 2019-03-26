f6a57e6465e7c86c4743a3695cee87c843be84d8
unknown <unknown@example.com>
2019-03-26 23:37:20 +0800

    ag files
    Check in ss/ag tool/ag files

--- autogenBandFreq/convertXlsx2Code.py
+++ autogenBandFreq/convertXlsx2Code.py
@@ -1,105 +1,33 @@
-import csv
+import xlrd
 
-filename = r'.\CDMA.csv'
-ag_filename = r'.\ag_cdma.txt'
+filename = r'.\summary.csv'
+ag_gsm_filename = r'.\ag_gsm.txt'
 
-isLTE = False
-isTDD = False
+class ULDL:
+    def __init__(self, chan_range, freq_base):
+        self.chan_range = chan_range
+        self.freq_base = freq_base
 
-with open(filename) as f, open(ag_filename, 'w') as f_ag:
-    reader = csv.DictReader(f)
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.dl = None
+        self.ul = None
+        self.type = 'default'
 
-    for row in reader:
-        isDLOnly = False
-        spacing = 0
+    def setDL(self, dl):
+        self.dl = dl
 
-        if 'DL Only' in row['Type']:
-            type = 'DL'
-            isDLOnly = True
-        elif 'TDD' in row['Type']:
-            type = 'tdd'
-        else:
-            type = 'default'
+    def setUL(self, ul):
+        self.ul = ul
 
-        band_info = '{\n  '
-        band_info += 'band: ' + row['Band'] + ', '
-        band_info += 'name: \'' + row['Band(str)'] + '\', '
-        band_info += 'type: \'' + type + '\', '
+class Tech:
+    def __init__(self, name):
+        self.name = name
+        self.band_list = []
 
-        ## spacing of different channels on BC5 of CDMA could be different
-        if ',' in row['Channel Spacing']:
-            band_info += 'spacing: [' + row['Channel Spacing'] + '], '
-        else:
-            band_info += 'spacing: ' + row['Channel Spacing'] + ', '
+    def addBand(self, band):
+        self.band_list.append(band)
 
-        if isLTE:
-            band_info += 'bandwidths: [' + row['Bandwidths'] + '], '
 
-        dl_chan_range = row['DL Channel Range']
-        dl_chan_begins = ''
-        dl_chan_ends = ''
-        if ',' in dl_chan_range:
-            new_chan_range = ''
-            first = True
-            for sub_str in dl_chan_range.split(','):
-                if first:
-                    first = False
-                    new_chan_range += '\"' + sub_str + '\"'
-                    dl_chan_begins += sub_str.split('-')[0]
-                    dl_chan_ends += sub_str.split('-')[1]
-                else:
-                    new_chan_range += ', \"' + sub_str + '\"'
-                    dl_chan_begins += ', ' + sub_str.split('-')[0]
-                    dl_chan_ends += ', ' + sub_str.split('-')[1]
-
-            dl_chan_range = new_chan_range
-        else:
-            dl_chan_begins = row['DL Channel Range'].split('-')[0]
-            dl_chan_ends = row['DL Channel Range'].split('-')[1]
-            dl_chan_range = '\"' + dl_chan_range + '\"'
-
-        ul_chan_range = row['UL Channel Range']
-        ul_chan_begins = ''
-        ul_chan_ends = ''
-        if ',' in ul_chan_range:
-            new_chan_range = ''
-            first = True
-            for sub_str in ul_chan_range.split(','):
-                if first:
-                    first = False
-                    new_chan_range += '\"' + sub_str + '\"'
-                    ul_chan_begins += sub_str.split('-')[0]
-                    ul_chan_ends += sub_str.split('-')[1]
-                else:
-                    new_chan_range += ', \"' + sub_str + '\"'
-                    ul_chan_begins += ', ' + sub_str.split('-')[0]
-                    ul_chan_ends += ', ' + sub_str.split('-')[1]
-
-            ul_chan_range = new_chan_range
-        else:
-            if isDLOnly or isTDD:
-                ul_chan_range = ''
-            else:
-                ul_chan_begins = row['UL Channel Range'].split('-')[0]
-                ul_chan_ends = row['UL Channel Range'].split('-')[1]
-                ul_chan_range = '\"' + ul_chan_range + '\"'
-
-        band_info += '\n  '
-        band_info += 'DL: { '
-        band_info += 'chanRange: [' + dl_chan_range + '], '
-        band_info += 'chanBegins: [' + dl_chan_begins + '], '
-        band_info += 'chanEnds: [' + dl_chan_ends + '], '
-        band_info += 'freqBase: [' + row['DL Frequency Base'] + '], '
-        band_info += '},'
-
-        band_info += '\n  '
-        band_info += 'UL: { '
-        band_info += 'chanRange: [' + ul_chan_range + '], '
-        band_info += 'chanBegins: [' + ul_chan_begins + '], '
-        band_info += 'chanEnds: [' + ul_chan_ends + '], '
-        band_info += 'freqBase: [' + row['UL Frequency Base'] + '], '
-        band_info += '},'
-        band_info += '\n},'
-        print(band_info)
-
-        f_ag.write(band_info)
+with open(filename) as f:
